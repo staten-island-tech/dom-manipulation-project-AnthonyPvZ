54683ae7a8ffc5a26de2e0a89f84189d98a72693
AnthonyPvZ <anthonypvz123@gmail.com>
2023-10-16 09:24:52 -0400

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,18 +1,34 @@
+// const DOMSelectors = {
+//     form: document.querySelector("#form"),
+//     firstname: document.querySelector(".first-name"),
+
+//     //grab the text input
+//     //grab ALL the h2s
+//     h2s:document.querySelectorAll("h2"),
+// };
+// DOMSelectors.form.addEventListener("submit", function(event){
+//     event.preventDefault();
+//     console.log(DOMSelectors.firstname.value);
+//     DOMSelectors.h2s.forEach(
+//         (el) => el.textContent = DOMSelectors.firstname.value
+//     );
+// });
+
+// console.log(DOMSelectors.firstname);
+// console.log(DOMSelectors.h2s);
+
 const DOMSelectors = {
-    form: document.querySelector("#form"),
-    firstname: document.querySelector(".first-name"),
 
-    //grab the text input
-    //grab ALL the h2s
-    h2s:document.querySelectorAll("h2"),
-};
-DOMSelectors.form.addEventListener("submit", function(event){
-    event.preventDefault();
-    console.log(DOMSelectors.firstname.value);
-    DOMSelectors.h2s.forEach(
-        (el) => el.textContent = DOMSelectors.firstname.value
-    );
-});
+}
+function Zombie(health, speed){
+    let.health = health;
+    let.speed = speed;
+}
+let Broodzombie = new Zombie("200", 16)
+let fastie = new Zombie("100", 24)
+console.log(fastie.health)
 
-console.log(DOMSelectors.firstname);
-console.log(DOMSelectors.h2s);
+function inject(zom)
+for
+
+// zombatar outline - have user input stat information, have javascript data on the type of zombie they made based on stats pushed into the html 
