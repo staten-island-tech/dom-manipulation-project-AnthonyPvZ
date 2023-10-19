f63d7ba0affd19c5c6e88ab5942ecd59c54ed2c4
AnthonyPvZ <anthonypvz123@gmail.com>
2023-10-19 09:21:56 -0400

    Actually working

--- dom.js
+++ dom.js
@@ -1,34 +1,27 @@
-// const DOMSelectors = {
-//     form: document.querySelector("#form"),
-//     firstname: document.querySelector(".first-name"),
-
-//     //grab the text input
-//     //grab ALL the h2s
-//     h2s:document.querySelectorAll("h2"),
-// };
-// DOMSelectors.form.addEventListener("submit", function(event){
-//     event.preventDefault();
-//     console.log(DOMSelectors.firstname.value);
-//     DOMSelectors.h2s.forEach(
-//         (el) => el.textContent = DOMSelectors.firstname.value
-//     );
-// });
-
-// console.log(DOMSelectors.firstname);
-// console.log(DOMSelectors.h2s);
-
 const DOMSelectors = {
+    form: document.querySelector("#form"),
+    firstname: document.querySelector(".first-name"),
 
-}
-function Zombie(health, speed){
-    let.health = health;
-    let.speed = speed;
-}
-let Broodzombie = new Zombie("200", 16)
-let fastie = new Zombie("100", 24)
-console.log(fastie.health)
+    //grab the text input
+    //grab ALL the h2s
+    h2s:document.querySelectorAll("h2"),
+    wipe: document.querySelector("#clear")
+};
+DOMSelectors.form.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log(DOMSelectors.firstname.value);
+    DOMSelectors.h2s.forEach(
+        (el) => el.textContent = DOMSelectors.firstname.value
+    );
+    document.getElementById("form").reset();
+});
+console.log(DOMSelectors.firstname);
+console.log(DOMSelectors.h2s);
 
-function inject(zom)
-for
-
+DOMSelectors.wipe.addEventListener("reset", function(wipe){
+wipe.preventDefault();
+DOMSelectors.h2s.forEach(
+    (el) =>el.textContent = DOMSelectors.firstname.value.reset()
+);
+});
 // zombatar outline - have user input stat information, have javascript data on the type of zombie they made based on stats pushed into the html 
