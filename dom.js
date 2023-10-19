35e67236876597eb67227a92ceab560ba9149207
AnthonyPvZ <anthonypvz123@gmail.com>
2023-10-19 08:53:45 -0400

    Revert "attempt 1"
    This reverts commit 1717c317452ebad0e8608e21b4943f0ac46b8cba.

--- dom.js
+++ dom.js
@@ -16,35 +16,19 @@
 
 // console.log(DOMSelectors.firstname);
 // console.log(DOMSelectors.h2s);
-document.getElementById("speed-stat").value;
-document.getElementById("health-stat").value;
 
 const DOMSelectors = {
-    speed: document.querySelector("speed-stat"),
-    health: document.querySelector("health-stat"),
-    h2s:document.querySelectorAll("h2"),
-};
 
-DOMSelectors.form.addEventListener("submit", function(event){
-    event.preventDefault();
-    console.log(DOMSelectors.health-stat.value);
-    DOMSelectors.h2s.forEach(
-        (el) => el.textcontent = DOMSelectors.health.value
-    );
-});
-console.log(DOMSelectors.health-stat);
-console.log(DOMSelectors.h2s)
+}
+function Zombie(health, speed){
+    let.health = health;
+    let.speed = speed;
+}
+let Broodzombie = new Zombie("200", 16)
+let fastie = new Zombie("100", 24)
+console.log(fastie.health)
 
-// function Zombie(health, speed){
-//     let.health = health;
-//     let.speed = speed;
-// };
-// let Broodzombie = new Zombie("200", 16);
-// let fastie = new Zombie("100", 24);
-// console.log(fastie.health);
-
+function inject(zom)
+for
 
 // zombatar outline - have user input stat information, have javascript data on the type of zombie they made based on stats pushed into the html 
-
-
-// what we need to do - 
