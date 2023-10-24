59e6ab474f3490a9d5a38f5172b4f90f574025ce
AnthonyPvZ <anthonypvz123@gmail.com>
2023-10-24 09:24:36 -0400

    c

--- dom.js
+++ dom.js
@@ -5,8 +5,20 @@ const DOMSelectors = {
     //grab the text input
     //grab ALL the h2s
     h2s:document.querySelectorAll("h2"),
-    wipe: document.querySelector("#clear")
 };
+function maketest(){
+    console.log(DOMSelectors)
+    return DOMSelectors;
+};
+
+function addnote(test){
+    document
+        .querySelector(".test")
+        .insertAdjacentHTML(
+            "afterbegin",
+            '<div class="card"><div class="card-title">${test.name}</div></div>'
+        )
+}
 DOMSelectors.form.addEventListener("submit", function(event){
     event.preventDefault();
     console.log(DOMSelectors.firstname.value);
@@ -18,10 +30,3 @@ DOMSelectors.form.addEventListener("submit", function(event){
 console.log(DOMSelectors.firstname);
 console.log(DOMSelectors.h2s);
 
-DOMSelectors.wipe.addEventListener("reset", function(wipe){
-wipe.preventDefault();
-DOMSelectors.h2s.forEach(
-    (el) =>el.textContent = DOMSelectors.firstname.value.reset()
-);
-});
-// zombatar outline - have user input stat information, have javascript data on the type of zombie they made based on stats pushed into the html 
