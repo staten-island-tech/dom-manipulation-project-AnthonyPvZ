79627ebd61bf9763011f18b5f5523b2dbad098ae
AnthonyPvZ <anthonypvz123@gmail.com>
2023-10-25 23:16:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const DOMSelectors = {
     cardimage: document.getElementsByClassName("card-img"),
     cardtitle: document.getElementsByClassName("card-title"),
-    description: document.getElementsByClassName("card-desc")
+    description: document.querySelector(".card-desc").textContent,
+    remantle: document.querySelector("#remantle")
 }
+console.log(DOMSelectors.description);
+
+function changedesc(desc) {
+    desc.textContent = "This swift shotgun allows you to load up to four shells in the chamber at once. Deals bonus damage & knockback to airborne targets. Has innate electric slow.";
+}
+DOMSelectors.remantle.addEventListener("click", function(){
+    changedesc(DOMSelectors.description)
+})
