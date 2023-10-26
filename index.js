148873ea7f3dc7825f272fcb9f5d931bfdf62a21
AnthonyPvZ <anthonypvz123@gmail.com>
2023-10-25 21:39:57 -0400

    k

---
+++ index.js
@@ -0,0 +1,5 @@
+const DOMSelectors = {
+    cardimage: document.getElementsByClassName("card-img"),
+    cardtitle: document.getElementsByClassName("card-title"),
+    description: document.getElementsByClassName("card-desc")
+}
