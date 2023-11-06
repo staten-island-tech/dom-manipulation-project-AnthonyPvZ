0d3a376e0fc4c021ac1ce59573e9fd7a5f8607aa
AnthonyPvZ <anthonypvz123@gmail.com>
2023-11-06 09:23:54 -0500

    Update dom.js

--- dom.js
+++ dom.js
@@ -8,7 +8,6 @@ const DOMSelectors = {
     resetbutton: document.querySelector("#reset"),
     funnybutton: document.querySelector("#funny"),
     cards: document.querySelectorAll(".card"),
-    resetfun: document.querySelector("#resetfunny"),
 };
 
 console.log(DOMSelectors.firstname);
@@ -30,32 +29,16 @@ DOMSelectors.resetbutton.addEventListener("click", function(clear){
 DOMSelectors.funnybutton.addEventListener("click", function(funny){
     funny.preventDefault();
     DOMSelectors.h2s.forEach((value)=> 
-        value.insertAdjacentHTML("afterend", '<div class="card"><div class="card-title">return the slab</div></div>'));
+        value.insertAdjacentHTML("beforeend", '<div class="card"><div class="card-title">return the slab</div></div>'));
 });
-DOMSelectors.resetfun.addEventListener("click", function(reset){
-    reset.preventDefault();
-    DOMSelectors.cards.removeAttributeNS("return the slab")
-});
+function removefunny(){
+    let buttons = document.querySelectorAll("button");
+    buttons.forEach((btn)=>
+    btn.addEventListener("click", function (event) {
+        console.log(event.target.parentElement)
+    }))
+};
+removefunny();
+
+
 // https://stackoverflow.com/questions/4777077/removing-elements-by-class-name
-
-
-// for(i = 0; i <=7; i--3) {console.log(i);}
-// while (i<7){
-//     console.log(i);
-//     i++;
-// };
-// function linearSearch(num,arr){
-//     let found;
-//     //for loop is iteration
-//     for(let i=0; i < arr.length; i++){
-//         //evaluations if statements, are selection
-//         if (arr[i]=== num){
-//             found = arr[i]
-//             break;
-//         } else{
-//             console.log("still lookiong");
-//         }
-//     } if(found =null) {console.log("sorry no");}
-//     else{console.log(found);} return found;
-// }
-// linearSearch (5, numsArr)
