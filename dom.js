bf95be00585eb3f6b28f42f2f87fe2e3573c48ab
AnthonyPvZ <anthonypvz123@gmail.com>
2023-11-08 00:10:15 -0500

    styled

--- dom.js
+++ dom.js
@@ -4,52 +4,42 @@ const DOMSelectors = {
 
     //grab the text input
     //grab ALL the h2s
-    h2s:document.querySelectorAll("h2"),
+    h2s: document.querySelectorAll("h2"),
     resetbutton: document.querySelector("#reset"),
     funnybutton: document.querySelector("#funny"),
     cards: document.querySelectorAll(".card"),
-    // remove: document.querySelectorAll("#resetfunny")
+    h1s: document.querySelectorAll("h1"),
 };
 
 console.log(DOMSelectors.firstname);
 console.log(DOMSelectors.h2s);
 
-DOMSelectors.form.addEventListener("submit", function(event){
+DOMSelectors.form.addEventListener("submit", function (event) {
     event.preventDefault();
     console.log(DOMSelectors.firstname.value);
-    DOMSelectors.h2s.forEach(
+    DOMSelectors.h1s.forEach(
         (el) => el.textContent = DOMSelectors.firstname.value
     );
     document.getElementById("form").reset();
 });
-DOMSelectors.resetbutton.addEventListener("click", function(clear){
+DOMSelectors.resetbutton.addEventListener("click", function (clear) {
     clear.preventDefault();
     DOMSelectors.h2s.forEach(
-        (el)=> el.textContent = "");
+        (el) => el.textContent = "");
 });
-DOMSelectors.funnybutton.addEventListener("click", function(funny){
+DOMSelectors.funnybutton.addEventListener("click", function (funny) {
     funny.preventDefault();
-    DOMSelectors.h2s.forEach((value)=> 
-        value.insertAdjacentHTML("beforeend", 
-        `<div class="card">
-        <div class="card-title">return the slab</div>
-        <button type="button" class="resetfunny">Click to clear funny</button>
+    DOMSelectors.h2s.forEach((value) =>
+        value.insertAdjacentHTML("beforeend",
+            `<div class="card">
+        <div class="card-title">Submitted by Triv Triv Boy</div>
+        <video controls width="1000" src="Darth_Vader.mp4" type="video/mp4"></video>
+        <p></p>
+        <button type="button" class="resetfunny">Click to clear video</button>
         </div>`));
-        document.querySelectorAll(".resetfunny").forEach((remov)=>{
-    remov.addEventListener("click", function(erase){
-    erase.target.parentElement.remove()
-})
+    document.querySelectorAll(".resetfunny").forEach((remov) => {
+        remov.addEventListener("click", function (erase) {
+            erase.target.parentElement.remove()
+        })
+    });
 });
-
-
-});
-
-// https://stackoverflow.com/questions/4777077/removing-elements-by-class-name
-function removefunny(){
-    let buttons = document.querySelectorAll("button");
-    buttons.forEach((btn)=>
-    btn.addEventListener("click", function (event) {
-        console.log(event.target.parentElement)
-    }))
-};
-removefunny();
